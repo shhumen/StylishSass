2af34af66fba12f3aa264d9a3b57fe2883fce3c9
shhumen <mehtiyevashumen@gmail.com>
2024-02-07 15:27:51 +0400

    published

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,15 @@
-// node_modules klasörü içerisinden gulp kütüphanesini çağırıyoruz
 const { src, dest, watch, series } = require('gulp')
-
-const purgecss = require('gulp-purgecss')
-const cssnano = require('gulp-cssnano')
-
-// gulp-sass kütüphanesini çağırıyoruz
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
-// hangi dosyanın, hangi dosya ile değiştirileceğini belirtiyoruz
 function buildStyles() {
-  return src('sass/**/*.scss')
-    .pipe(sass())
-    .pipe(purgecss({ content: ['*.html'] }))
-    .pipe(cssnano())
-    .pipe(dest('css'))
+  return src('src/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(dest('dist'))
 }
-
-//  /* /**/ means folders in sass */   */
-
-// index.scss dosyasında değişiklik olduğunda buildStyles fonksiyonunu çalıştır
 
 function watchTask() {
-  return watch(['sass/**/*.scss', '*.html'], buildStyles)
+  watch(['src/**/*.scss'], buildStyles)
 }
 
-// default olarak çalıştırılacak fonksiyonu belirtiyoruz
 exports.default = series(buildStyles, watchTask)
